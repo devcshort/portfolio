253a3b12f53f4e230247950157f338e70278f849
chriss <13677134+devcshort@users.noreply.github.com>
2021-03-19 13:35:35 -0700

    initial commit

---
+++ pages/index.tsx
@@ -0,0 +1,12 @@
+import Head from 'next/head';
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>Create Next App</title>
+        <link rel="icon" type="image/png" href="/favicon.png" />
+      </Head>
+    </div>
+  );
+}
